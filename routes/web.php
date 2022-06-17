94fb57e20a36d7bd3129c516eef34d63461d924b
himanshu rahi <himans@calsob.com>
2022-06-17 16:30:41 +0530

    Request & Response

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,8 @@ Route::get('/', function () {
 })->name('home.index');
 
 Route::get('/posts', function() use($posts) {
+    // dd(request()->all());
+    dd(request()->query('page',1));
     return view('posts.index', compact('posts'));
 });
 
@@ -76,3 +78,23 @@ Route::view("contact", "contact.index")->name('contact.index');
 Route::get("/recent-posts/{days_ago?}", function ($days_ago = 10) {
     return "Days Ago: ".$days_ago;
 })->name('recent.index');
+
+
+Route::prefix('fun')->name('fun.')->group(function() use ($posts){
+    Route::get("responses", function () {
+        return response("himanshurahu", 200)
+        ->header('Content-Type', 'application/json')
+        ->cookie('name', 'himanshurahu', 3600);
+    });
+    Route::get("redirect", function(){
+        return redirect()->route('home.index');
+    });
+    
+    Route::get("/json", function() use ($posts){
+        return response()->json($posts);
+    });
+    
+    Route::get("/download", function(){
+        return response()->download(public_path('index.php'));
+    });
+});
